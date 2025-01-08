d98f200af5d7cd2d1fb070374123be7a9afb5bad
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-08 22:35:36 +0100

    MAJ R C-Ames > Fin

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -3,6 +3,7 @@
 const selRondes = $("#sel_rondes");
 
 const basePremier = $("#ste_marie_vertus_1");
+const finPremier = $("#fin_trio_1");
 const btnPremier = $("#dz_grains_1");
 
 // Variables
@@ -18,5 +19,6 @@ function resetAll()
     selRondes.selectedIndex = 0;
 
     basePremier.checked = true;
+    finPremier.checked = true;
     btnPremier.checked = true;
 }
