c5f9c4b3a0dcee35d987f4c443c2fd6e7b79825a
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-09 18:11:15 +0100

    MAJ Nv Dz-Sang

--- compile.py
+++ compile.py
@@ -27,13 +27,14 @@ profil = ["cv-imprime", "portfolio"]
 
 ## Prières
 
-prieres = ["chapelet", "ch-sang", "dizaines", "litanies"]
+prieres = ["chapelet", "ch-sang", "dizaines", "dz-sang", "litanies"]
 
 #convert_jinja_list("prieres", prieres)
 
 #convert_jinja_single("prieres/chapelet")
 convert_jinja_single("prieres/ch-sang")
 #convert_jinja_single("prieres/dizaines")
+convert_jinja_single("prieres/dz-sang")
 #convert_jinja_single("prieres/litanies")
 
 ## Exercices
