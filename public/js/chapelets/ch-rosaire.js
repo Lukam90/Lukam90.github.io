3350e138179b78ddeeec9860d6c06335a387224c
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-11 23:25:34 +0100

    MAJ R Chapelet > Raccourcis & Options

--- public/js/chapelets/ch-rosaire.js
+++ public/js/chapelets/ch-rosaire.js
@@ -72,8 +72,36 @@ function majInfos()
 
 /* Raccourcis */
 
+// Sélection d'un type
+
+function selectType(index)
+{
+    goTo("#mysteres");
+
+    selType.focus();
+    selType.selectedIndex = index;
+
+    majMysteres();
+}
+
+// Evénements
+
 document.addEventListener("keydown", e => {
+    numKey = parseInt(e.key);
+
     if (e.key == "l")   cbLatin.click();
 
     if (e.key == "m")   goTo("#mysteres");
+
+    if (e.key == "a")   selectType(0);
+    if (e.key == "b")   selectType(1);
+    if (e.key == "c")   selectType(2);
+    if (e.key == "d")   selectType(3);
+
+    if (numKey >= 1 && numKey <= 5) {
+        selMysteres.focus();
+        selMysteres.selectedIndex = numKey - 1;
+
+        majInfos();
+    }
 });
