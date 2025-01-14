6c7b9cb1a8a2568aba1d3e19c1b51d6d1a11a003
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-14 18:59:40 +0100

    MAJ R Chapelet > Dizaine

--- public/js/chapelets/ch-rosaire.js
+++ public/js/chapelets/ch-rosaire.js
@@ -3,38 +3,21 @@
 const checkboxes = $all("input[type='checkbox']");
 
 const trioPremier = $("#base_trio_1");
-
-const jPremier = $("#dz_grains_joyeux_1");
-const lPremier = $("#dz_grains_lumineux_1");
-const dPremier = $("#dz_grains_douloureux_1");
-const gPremier = $("#dz_grains_glorieux_1");
+const dzPremier = $("#dz_grains_1");
 
 /* Fonctions */
 
 // Réinitialisation des cases à cocher et des boutons radio
 
-function resetRadioButtons()
+function resetAll()
 {
     trioPremier.checked = true;
+    dzPremier.checked = true;
 
-    jPremier.checked = true;
-    lPremier.checked = true;
-    dPremier.checked = true;
-    gPremier.checked = true;
-}
-
-function resetCheckboxes()
-{
     for (let cb of checkboxes)
     {
         cb.checked = false;
     }
-}
-
-function resetAll()
-{
-    resetRadioButtons();
-    resetCheckboxes();
 }
 
 /* Raccourcis */
