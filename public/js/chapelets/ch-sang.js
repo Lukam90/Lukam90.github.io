ae515a2ac3d6ca98533943f66c51afd76b37822a
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-14 14:12:48 +0100

    MAJ R C-Sang > Douzaines

---
+++ public/js/chapelets/ch-sang.js
@@ -0,0 +1,10 @@
+/* Raccourcis */
+
+document.addEventListener("keydown", e => {
+    if (e.key == "0")   goTo("#");
+    if (e.key == "1")   goTo("#p1");
+    if (e.key == "2")   goTo("#p2");
+    if (e.key == "3")   goTo("#p3");
+    if (e.key == "4")   goTo("#p4");
+    if (e.key == "5")   goTo("#p5");
+});
