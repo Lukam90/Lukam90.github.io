f764b8d4de1d6699d010af7eaae908b51cba9246
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-14 14:57:36 +0100

    MAJ R C-Sang > Fin

--- public/js/chapelets/ch-sang.js
+++ public/js/chapelets/ch-sang.js
@@ -7,4 +7,6 @@ document.addEventListener("keydown", e => {
     if (e.key == "3")   goTo("#p3");
     if (e.key == "4")   goTo("#p4");
     if (e.key == "5")   goTo("#p5");
+
+    if (e.key == "f" || e.key == "6")   goTo("#fin");
 });
