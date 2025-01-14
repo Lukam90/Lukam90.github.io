43221dd7e09cfc5e3342ec0d479b26f32a074665
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-14 22:20:51 +0100

    MAJ C-Rosaire > Selection

--- public/js/chapelets/ch-rosaire.js
+++ public/js/chapelets/ch-rosaire.js
@@ -2,14 +2,44 @@
 
 const checkboxes = $all("input[type='checkbox']");
 
+const blocks = $all(".mysteres");
+
+const selTypes = $("#sel_types");
+
 const trioPremier = $("#base_trio_1");
 const dzPremier = $("#dz_grains_1");
 
+let block;
+
+/* Variables */
+
+const types = ["joyeux", "lumineux", "douloureux", "glorieux"];
+
+let index = 0;
+let type = "";
+
 /* Fonctions */
+
+setBlock();
+
+// MAJ du bloc
+
+function setBlock()
+{
+    index = selTypes.selectedIndex;
+    type = types[index];
+
+    block = $("#m_" + type);
+
+    hideAll(blocks);
+    showBlock(block);
+
+    resetButtons();
+}
 
 // Réinitialisation des cases à cocher et des boutons radio
 
-function resetAll()
+function resetButtons()
 {
     trioPremier.checked = true;
     dzPremier.checked = true;
@@ -20,15 +50,19 @@ function resetAll()
     }
 }
 
+// Réinitialisation du chapelet
+
+function resetAll()
+{
+    selTypes.selectedIndex = 0;
+
+    setBlock();
+}
+
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
     if (e.key == "0")   goTo("#");
-
-    if (e.key == "j" || e.key == "1")   goTo("#m_joyeux");
-    if (e.key == "l" || e.key == "2")   goTo("#m_douloureux");
-    if (e.key == "d" || e.key == "3")   goTo("#m_lumineux");
-    if (e.key == "g" || e.key == "4")   goTo("#m_glorieux");
 
     if (e.key == "r")   resetAll();
 });
