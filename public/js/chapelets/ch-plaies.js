048ffa1b904897a11d155558e51d9684c5f05230
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-19 20:54:13 +0100

    MAJ R C-Plaies

--- public/js/chapelets/ch-plaies.js
+++ public/js/chapelets/ch-plaies.js
@@ -1,32 +1,8 @@
 /* Eléments */
 
-const rdMysteres = $name("rd_mysteres");
-
-const btnPremier = rdMysteres[0];
-
-const pMystere = $("#mystere");
-
-/* Variables */
-
-const plaies = ["1. les mains", "2. les pieds", "3. le dos", "4. la tête", "5. le coeur"];
-
-let index = 0, numKey = 0;
-let mystere = "";
+const btnPremier = $("#p1");
 
 /* Fonctions */
-
-resetAll();
-
-// MAJ du mystère
-
-function majMystere(number)
-{
-    index = parseInt(number) - 1;
-
-    mystere = plaies[index];
-
-    pMystere.textContent = mystere;
-}
 
 // Réinitialisation du chapelet
 
@@ -34,9 +10,7 @@ function resetAll()
 {
     goTo("#");
 
-    majMystere(1);
-
-    checkButton(btnPremier);
+    checkFirst();
 }
 
 /* Raccourcis */
