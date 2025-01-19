69a483d119a12377f17fefa0dea22d7f502103e2
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-19 19:03:08 +0100

    MAJ R C-Ames > Tableau

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -1,43 +1,11 @@
 /* Eléments */
 
-const rdNiveaux = $name("rd_niveaux");
-
-const btnPremier = rdNiveaux[0];
+const btnPremier = $("#nv1");
 
 const basePremier = $("#base_trio_1");
 const nvPremier = $("#nv_trio_1");
 
-const pNiveau = $("#niveau");
-
-/* Variables */
-
-const niveaux = [
-    "1. Crainte de Dieu", "2. Chasteté", "3. Foi",
-    "4. Espérance", "5. Obéissance", "6. Patience",
-    "7. Piété", "8. Chasteté", "9. Prudence",
-    "10. Force", "11. Tempérance", "12. Pureté",
-    "13. Justice"
-];
-
-let index = 0;
-let niveau = "";
-
 /* Fonctions */
-
-resetAll();
-
-// MAJ du niveau
-
-function majNiveau(number)
-{
-    index = parseInt(number) - 1;
-
-    niveau = niveaux[index];
-
-    pNiveau.textContent = niveau;
-
-    resetButtons();
-}
 
 // Réinitialisation des boutons radio
 
@@ -53,14 +21,15 @@ function resetAll()
 {
     goTo("#");
 
-    majNiveau(1);
+    resetButtons();
 
-    checkButton(btnPremier);
+    checkFirst();
 }
 
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
+    // Réinitialisation
     if (e.key == "0")   resetAll();
 
     // Ancres
@@ -69,5 +38,5 @@ document.addEventListener("keydown", e => {
     if (e.key == "g")   checkButton(basePremier);
     if (e.key == "a")   goTo("#ave");
     if (e.key == "m")   goTo("#medaille");
-    if (e.key == "m")   goTo("#rondes");
+    if (e.key == "r")   goTo("#rondes");
 });
